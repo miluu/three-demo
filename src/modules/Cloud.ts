f83c326ade50246d5074b391a19d9c415e4c447c
miluu <hyxiong@qq.com>
2017-05-10 22:59:42 +0800

    Cloud

---
+++ src/modules/Cloud.ts
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+import {COLORS} from '../const';
+
+export default class Cloud {
+  mesh: THREE.Mesh;
+  constructor () {
+    let geom = new THREE.BoxGeometry(20, 20, 20);
+    let mat = new THREE.MeshPhongMaterial({
+      color: COLORS.white
+    });
+    let nBlocs = 3 + Math.floor(Math.random() * 3);
+    for (let i = 0; i < nBlocs; i++) {
+      let m = new THREE.Mesh(geom, mat);
+      m.position.x = i * 15;
+      m.position.y = Math.random() * 10;
+      m.position.z = Math.random() * 10;
+      m.rotation.z = Math.random() * Math.PI * 2;
+      m.rotation.y = Math.random() * Math.PI * 2;
+      let s = 0.1 + Math.random() * 0.9;
+      m.scale.set(s, s, s);
+      m.castShadow = true;
+      m.receiveShadow = true;
+      this.mesh.add(m);
+    }
+  }
+}
